87a50c191a5673c4cf7858a241f5834bf064de41
Jay Sivakumaran <jsivakumaran@outlook.com>
2015-10-12 22:49:49 -0700

    Add directory structure to simple Ng2 App

---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({selector: 'my-app'})
+@View({template: '<h1> My First Ng2 app</h1>'})
+class AppComponent {}
+
+bootstrap(AppComponent);
